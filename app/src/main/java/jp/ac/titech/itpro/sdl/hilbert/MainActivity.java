f2ce87b62921579c33bc5292c90f321c44e61984
Yamada <defdrhrk602@gmail.com>
2016-04-29 16:35:11 +0900

    modified

--- app/src/main/java/jp/ac/titech/itpro/sdl/hilbert/MainActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/hilbert/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
     private TextView levelView;
@@ -11,20 +12,64 @@ public class MainActivity extends AppCompatActivity {
     private final static int MAX_N = 9;
     private int n = 1;
 
+    private static final String TAG = "MainActivity";
+    private final static String KEY_N = "MainActivity.n";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate");
         setContentView(R.layout.activity_main);
-        levelView = (TextView)findViewById(R.id.level_view);
-        hilbertView = (HilbertView)findViewById(R.id.hilbert_view);
+        setContentView(R.layout.activity_main);
+        levelView = (TextView) findViewById(R.id.level_view);
+        hilbertView = (HilbertView) findViewById(R.id.hilbert_view);
+
+        if (savedInstanceState != null)
+            n = savedInstanceState.getInt(KEY_N);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(KEY_N, n);
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Log.d(TAG, "onStart");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+        Log.d(TAG, "onResume");
         display();
     }
-
     public void onClickDec(View v) {
         if (n > 1) n--;
         display();
